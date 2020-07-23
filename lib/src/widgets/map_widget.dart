3fb29875ec5085a923daf2ca7008c8cec0c8772f
Crdzbird <luisalfonsocb83@gmail.com>
2020-07-23 17:33:15 -0600

    Added main doctor on dropdown in new appointment map

---
+++ lib/src/widgets/map_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MapWidget extends StatefulWidget {
+  @override
+  _MapWidgetState createState() => _MapWidgetState();
+}
+
+class _MapWidgetState extends State<MapWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/src/widgets/new_appointment_map_widget.dart
+++ lib/src/widgets/new_appointment_map_widget.dart
@@ -7,8 +7,13 @@ import 'package:medical_app/src/widgets/find_nearby_doctors_widget.dart';
 import 'package:medical_app/src/widgets/new_appointment_map_header_widget.dart';
 import 'package:provider/provider.dart';
 
-const double initialPercentage = 0.15;
-const double maxSize = 0.8;
+const double minHeight = 120;
+const double iconStartSize = 44;
+const double iconEndSize = 120;
+const double iconStartMarginTop = 36;
+const double iconEndMarginTop = 80;
+const double iconsVerticalSpacing = 24;
+const double iconsHorizontalSpacing = 16;
 
 class NewAppointmentMapWidget extends StatefulWidget {
   @override
@@ -16,73 +21,100 @@ class NewAppointmentMapWidget extends StatefulWidget {
       _NewAppointmentMapWidgetState();
 }
 
-class _NewAppointmentMapWidgetState extends State<NewAppointmentMapWidget> {
-  double percentage = 0.15;
-  double scaledPercentage = 0.0;
+class _NewAppointmentMapWidgetState extends State<NewAppointmentMapWidget>
+    with SingleTickerProviderStateMixin {
   DoctorsProvider provider;
+  AnimationController _controller;
+  double get maxHeight => MediaQuery.of(context).size.height * 0.8;
+  double get headerTopMargin =>
+      lerp(20, 20 + MediaQuery.of(context).padding.top);
+  double get headerFontSize => lerp(14, 24);
+  double get itemBorderRadius => lerp(8, 24);
+  double get iconLeftBorderRadius => itemBorderRadius;
+  double get iconRightBorderRadius => lerp(8, 0);
+  double get iconSize => lerp(iconStartSize, iconEndSize);
+  double iconTopMargin(int index) =>
+      lerp(iconStartMarginTop,
+          iconEndMarginTop + index * (iconsVerticalSpacing + iconEndSize)) +
+      headerTopMargin;
+  double iconLeftMargin(int index) =>
+      lerp(index * (iconsHorizontalSpacing + iconStartSize), 0);
 
   @override
-  Widget build(BuildContext context) {
-    provider = Provider.of<DoctorsProvider>(context);
-    final size = MediaQuery.of(context).size;
-    final paddingTop = MediaQuery.of(context).padding.top;
-    percentage = initialPercentage;
-    return Positioned.fill(
-      child: DraggableScrollableSheet(
-        minChildSize: initialPercentage,
-        maxChildSize: maxSize,
-        initialChildSize: initialPercentage,
-        builder: (context, scrollController) {
-          return AnimatedBuilder(
-            animation: scrollController,
-            builder: (context, child) {
-              if (scrollController.hasClients)
-                percentage = (scrollController.position.viewportDimension /
-                    (size.height * maxSize));
-              scaledPercentage = (percentage * maxSize - initialPercentage) /
-                  (1 - initialPercentage);
-              return Container(
-                padding: EdgeInsets.only(left: 32),
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(247, 247, 247, 0.9),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 10,
-                      blurRadius: 7,
-                      offset: Offset(0, 2),
-                    ),
-                  ],
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(32),
-                  ),
-                ),
-                child: Stack(
-                  children: [
-                    Opacity(
-                      opacity: percentage == 1 ? 1 : 0,
-                      child: Container(),
-                    ),
-                    NewAppointmentMapHeaderWidget(
-                      fontSize: 14 + percentage * 2,
-                      topMargin: 10 + percentage * paddingTop,
-                    ),
-                    FindNearbyDoctorsWidget(
-                      percentage: percentage,
-                    ),
-                  ],
-                ),
-              );
-            },
-          );
-        },
-      ),
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 600),
     );
   }
 
   @override
   void dispose() {
+    _controller.dispose();
     provider?.dispose();
     super.dispose();
+  }
+
+  double lerp(double min, double max) =>
+      lerpDouble(min, max, _controller.value);
+
+  void _toggle() {
+    final bool isOpen = _controller.status == AnimationStatus.completed;
+    _controller.fling(velocity: isOpen ? -2 : 2);
+  }
+
+  void _handleDragUpdate(DragUpdateDetails details) {
+    _controller.value -= details.primaryDelta / maxHeight;
+  }
+
+  void _handleDragEnd(DragEndDetails details) {
+    if (_controller.isAnimating ||
+        _controller.status == AnimationStatus.completed) return;
+
+    final double flingVelocity =
+        details.velocity.pixelsPerSecond.dy / maxHeight;
+    if (flingVelocity < 0.0)
+      _controller.fling(velocity: math.max(2.0, -flingVelocity));
+    else if (flingVelocity > 0.0)
+      _controller.fling(velocity: math.min(-2.0, -flingVelocity));
+    else
+      _controller.fling(velocity: _controller.value < 0.5 ? -2.0 : 2.0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    provider = Provider.of<DoctorsProvider>(context);
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Positioned(
+          height: lerp(minHeight, maxHeight),
+          left: 0,
+          right: 0,
+          bottom: 0,
+          child: GestureDetector(
+            onTap: _toggle,
+            onVerticalDragUpdate: _handleDragUpdate,
+            onVerticalDragEnd: _handleDragEnd,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              decoration: const BoxDecoration(
+                color: Color.fromRGBO(247, 247, 247, 0.9),
+                borderRadius: BorderRadius.vertical(top: Radius.circular(32)),
+              ),
+              child: Stack(
+                children: <Widget>[
+                  NewAppointmentMapHeaderWidget(
+                    fontSize: headerFontSize,
+                    topMargin: headerTopMargin,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
